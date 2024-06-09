8de3faf3df36020b18b64f3f903469af2400ffc4
Vitor Rosa <vitor.rosa.dev@outlook.com>
2024-06-09 05:19:21 -0300

    X-devs final

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+const btnAvanvar = document.getElementById("btn-avancar")
+const btnVoltar = document.getElementById("btn-voltar")
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
+
+btnAvanvar.addEventListener("click", function () {
+    if (cartaoAtual === cartoes.length - 1) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual++;
+    mostrarCartao();
+});
+
+btnVoltar.addEventListener("click", function () {
+    if (cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual--;
+    mostrarCartao();
+})
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+
+    cartaoSelecionado.classList.remove("selecionado");
+}
